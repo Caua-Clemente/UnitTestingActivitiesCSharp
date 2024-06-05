1b17bc356a6fbbe2cf6bc345ed269d1d21bbacbf
Caua-Clemente <cauaclementelima@gmail.com>
2024-06-05 17:01:06 -0300

    Alterando gitignore e subindo 6 e 7

---
+++ Atividade06.Tests/PointTests.cs
@@ -0,0 +1,48 @@
+﻿using Atividade06;
+using Xunit;
+
+namespace Atividade06.Tests
+{
+    public class PointTests
+    {
+        [Fact]
+        public void DistanceTo_ReturnsCorrectDistance(){
+            var pointOrigin = new Point(10, 10);
+            var pointFinal = new Point(13, 14);
+            var distance = pointOrigin.DistanceTo(pointFinal);
+
+            Assert.Equal(5, distance);
+        }
+
+        [Fact]
+        public void DistanceTo_TwoEqualPoints_ReturnsDistanceZero()
+        {
+            var pointOrigin = new Point(10, 10);
+            var pointFinal = new Point(10, 10);
+            var distance = pointOrigin.DistanceTo(pointFinal);
+
+            Assert.Equal(0, distance);
+        }
+
+        [Fact]
+        public void DistanceTo_PositiveAndNegativePoints_ReturnsCorrectDistance()
+        {
+            var pointOrigin = new Point(10, 10);
+            var pointFinal = new Point(-20, -30);
+            var distance = pointOrigin.DistanceTo(pointFinal);
+
+            Assert.Equal(50, distance);
+        }
+
+        [Fact]
+        public void DistanceTo_TwoNegativePoints_ReturnsCorrectDistance()
+        {
+            var pointOrigin = new Point(-2, -2);
+            var pointFinal = new Point(-7, -14);
+            var distance = pointOrigin.DistanceTo(pointFinal);
+
+            Assert.Equal(13, distance);
+        }
+    }
+}
+
---
+++ Atividade07.Tests/UserServiceTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Atividade07.UserServiceTests
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public void SaveUser_ReturnsNoException()
+        {
+            var user = new User("Caua", "cauaclemente@gmail.com");
+            var database = new Database();
+            var service = new UserService(database);
+
+            var exception = Record.Exception(() => service.SaveUser(user));
+
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void SaveUser_EmptyOrNullName_ReturnsException()
+        {
+            var userEmpty = new User("", "cauaclemente@gmail.com");
+            var userNull = new User(null , "cauaclemente@gmail.com");
+            var database = new Database();
+            var service = new UserService(database);
+
+            Assert.Throws<ArgumentException> (() => service.SaveUser(userEmpty));
+            Assert.Throws<ArgumentException> (() => service.SaveUser(userNull));
+        }
+
+        [Fact]
+        public void SaveUser_EmptyOrNullEmail_ReturnsException()
+        {
+            var userEmpty = new User("Caua", "");
+            var userNull = new User("Caua" , null);
+            var database = new Database();
+            var service = new UserService(database);
+
+            Assert.Throws<ArgumentException> (() => service.SaveUser(userEmpty));
+            Assert.Throws<ArgumentException> (() => service.SaveUser(userNull));
+        }
+    }
+}
+
