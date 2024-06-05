dfba361e0a629b6a82e76585b5a976ef6af1dc92
Caua-Clemente <cauaclementelima@gmail.com>
2024-06-05 19:02:31 -0300

    feat/atividades-8-9

---
+++ Atividade08.Tests/StatisticsTests.cs
@@ -0,0 +1,68 @@
+﻿namespace Atividade08.Tests
+{
+    public class StatisticsTests
+    {
+        [Fact]
+        public void CalculateAverage_ReturnsCorrectAverage()
+        {
+            var statistics = new Statistics();
+            var numberList = new List<int> {1, 2, 3, 4, 5};
+
+            Assert.Equal(3, statistics.CalculateAverage(numberList));
+        }
+
+        [Fact]
+        public void CalculateAverage_NegativeNumbers_ReturnsCorrectAverage()
+        {
+            var statistics = new Statistics();
+            var numberList = new List<int> {-1, -2, -3, -4, -5};
+
+            Assert.Equal(-3, statistics.CalculateAverage(numberList));
+        }
+
+        [Fact]
+        public void CalculateAverage_SameNumbers_ReturnsCorrectAverage()
+        {
+            var statistics = new Statistics();
+            var numberList = new List<int> {10, 10, 10, 10, 10};
+
+            Assert.Equal(10, statistics.CalculateAverage(numberList));
+        }
+
+        [Fact]
+        public void CalculateAverage_ListWithOnlyZero_ReturnsCorrectAverage()
+        {
+            var statistics = new Statistics();
+            var numberList = new List<int> {0};
+
+            Assert.Equal(0, statistics.CalculateAverage(numberList));
+        }
+
+        [Fact]
+        public void CalculateAverage_VariousNumbers_ReturnsCorrectAverage()
+        {
+            var statistics = new Statistics();
+            var numberList = new List<int> {-1, 10, -3, 20, 50, -100, 2, 77, -45, 0};
+
+            Assert.Equal(1, statistics.CalculateAverage(numberList));
+        }
+
+        [Fact]
+        public void CalculateAverage_ZeroCountList_ReturnsException()
+        {
+            var statistics = new Statistics();
+            var numberList = new List<int> {};
+
+            Assert.Throws<ArgumentException>(() => statistics.CalculateAverage(numberList));
+        }
+
+        [Fact]
+        public void CalculateAverage_NullList_ReturnsException()
+        {
+            var statistics = new Statistics();
+
+            Assert.Throws<ArgumentException>(() => statistics.CalculateAverage(null));
+        }
+    }
+}
+
---
+++ Atividade09.Tests/ItemCollectionTests.cs
@@ -0,0 +1,88 @@
+﻿using NSubstitute.Core;
+
+namespace Atividade09.Tests
+{
+    public class ItemCollectionTests
+    {
+        [Fact]
+        public void AddItem_AddItemToList_WhenItemIsValid()
+        {
+            var itemCollection = new ItemCollection();
+            var item = new Item("Nome do item");
+            
+            itemCollection.AddItem(item);
+            Assert.Contains(item, itemCollection.GetItems());
+        }
+
+        [Fact]
+        public void AddItem_ThrowsArgumentException_WhenItemIsNull()
+        {
+            var itemCollection = new ItemCollection();
+
+            Assert.Throws<ArgumentException>(()=> itemCollection.AddItem(null));
+        }
+
+        [Fact]
+        public void RemoveItem_RemoveItemFromList_WhenItemExists()
+        {
+            var itemCollection = new ItemCollection();
+            var item = new Item("Nome do item");
+            itemCollection.AddItem(item);
+            itemCollection.RemoveItem(item);
+
+            Assert.Equal(new List<Item>(), itemCollection.GetItems());
+        }
+
+        [Fact]
+        public void RemoveItem_RemoveItemFromPopulatedList_WhenItemExists()
+        {
+            var itemCollection = new ItemCollection();
+            var itemA = new Item("Nome A");
+            var itemB = new Item("Nome B");
+            var itemC = new Item("Nome C");
+            itemCollection.AddItem(itemA);
+            itemCollection.AddItem(itemB);
+            itemCollection.AddItem(itemC);
+            itemCollection.RemoveItem(itemB);
+
+            var collectionAC = new ItemCollection();
+            collectionAC.AddItem(itemA);
+            collectionAC.AddItem(itemC);
+            
+            Assert.Equal(collectionAC.GetItems(), itemCollection.GetItems());
+        }
+
+        [Fact]
+        public void RemoveItem_ThrowsArgumentException_WhenItemDoesNotExist()
+        {
+            var itemCollection = new ItemCollection();
+            var item = new Item("Nome do item");
+
+            Assert.Throws<ArgumentException>(() => itemCollection.RemoveItem(item));
+        }
+
+        [Fact]
+        public void GetItems_ReturnListOfItems_WhenListHaveItems()
+        {
+            var itemCollection = new ItemCollection();
+            var itemA = new Item("Nome A");
+            var itemB = new Item("Nome B");
+            var itemC = new Item("Nome C");
+            itemCollection.AddItem(itemA);
+            itemCollection.AddItem(itemB);
+            itemCollection.AddItem(itemC);
+
+            var listOfItems = new List<Item>{itemA, itemB, itemC};
+            Assert.Equal(listOfItems, itemCollection.GetItems());
+        }
+
+        [Fact]
+        public void GetItems_ReturnEmptyList_WhenListIsEmpty()
+        {
+            var itemCollection = new ItemCollection();
+            var listOfItems = new List<Item>();
+            Assert.Equal(listOfItems, itemCollection.GetItems());
+        }
+    }
+}
+
